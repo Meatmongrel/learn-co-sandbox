0e3662761e1e7b8138b61e088740cc46adf3a243
Andrew Thiessen <squaids449@gmail.com>
2019-08-27 15:30:40 +0000

    Automatically backed up by Learn

--- TEST.rb
+++ TEST.rb
@@ -1,11 +1,12 @@
-# def reverse_each_word(sentence)
-#   arr = sentence.split(" ")
-#   rev = arr.collect { |w| w.reverse!} 
-#   newrev = rev.join(" ")
-#   return newrev
-# end
+def reverse_each_word(sentence)
+  arr = sentence.split(" ")
+  rev = arr.collect { |w| w.reverse!} 
+  newrev = rev.join(" ")
+  return newrev
+end
 
-# p reverse_each_word("This is a sentence")
+p reverse_each_word("This is a sentence")
 
-array = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
+# array = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
 
+# def firs
