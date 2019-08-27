41926c8766490378f8fa002922414c2437a4d050
Andrew Thiessen <squaids449@gmail.com>
2019-08-27 15:54:30 +0000

    Automatically backed up by Learn

--- TEST.rb
+++ TEST.rb
@@ -1,38 +1,38 @@
 
-# def reverse_each_word(sentence)
-#   arr = sentence.split(" ")
-#   rev = arr.collect { |w| w.reverse!} 
-#   newrev = rev.join(" ")
-#   return newrev
+# # def reverse_each_word(sentence)
+# #   arr = sentence.split(" ")
+# #   rev = arr.collect { |w| w.reverse!} 
+# #   newrev = rev.join(" ")
+# #   return newrev
+# # end
+
+# # p reverse_each_word("This is a sentence")
+
+# array = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
+
+# def firstLetter(array)
+#   new = []
+#   array.each do |word|
+#     if word[0] == "a"
+#       new << word
+#     end
+#   end
+#   new
 # end
 
-# p reverse_each_word("This is a sentence")
+# string = "This is a sentence for a test."
 
-array = ["apple", "pear", "face", "champagne", "palm tree", "aardvark", "pineapple"]
+# def wordCount(sentence)
+#   new = sentence.split(" ")
+#   num = 0
+#   new.each do |word|
+#     num += 1
+#   end
+#   num
+# end
 
-def firstLetter(array)
-  new = []
-  array.each do |word|
-    if word[0] == "a"
-      new << word
-    end
-  end
-  new
-end
-
-string = "This is a sentence for a test."
-
-def wordCount(sentence)
-  new = sentence.split(" ")
-  num = 0
-  new.each do |word|
-    num += 1
-  end
-  num
-end
-
-p firstLetter(array)
-p wordCount(string)
+# p firstLetter(array)
+# p wordCount(string)
 foods = {"pie" => "delicious", "broccoli" => "not delicious",
 "carrots" => "not delicious", "apples" => "delicious",
 "peanut butter" => "delicious"}
