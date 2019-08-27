2f9ad26dfebd8e0630e381f2683ebea8d7466fd4
Andrew Thiessen <squaids449@gmail.com>
2019-08-27 15:29:01 +0000

    Automatically backed up by Learn

--- TEST.rb
+++ TEST.rb
@@ -1,8 +1,9 @@
-def reverse_each_word(sentence)
-  arr = sentence.split(" ")
-  rev = arr.collect { |w| w.reverse!} 
-  newrev = rev.join(" ")
-  return newrev
-end
+# def reverse_each_word(sentence)
+#   arr = sentence.split(" ")
+#   rev = arr.collect { |w| w.reverse!} 
+#   newrev = rev.join(" ")
+#   return newrev
+# end
 
-p reverse_each_word("This is a sentence")
+# p reverse_each_word("This is a sentence")
+
